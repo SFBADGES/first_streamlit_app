68f13807f19a307248e6356a34518cb866025b53
SFBADGES <128747881+SFBADGES@users.noreply.github.com>
2023-03-23 16:46:28 -0400

    Update streamlit_app.py
    added item to menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,6 @@ streamlit.header('Breakfast Menu')
 
 streamlit.text('Omega 3 & Blue Berry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
  
 
