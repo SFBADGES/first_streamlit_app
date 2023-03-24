7916d389da841a62e1bc06f896271990e178938b
SFBADGES <128747881+SFBADGES@users.noreply.github.com>
2023-03-24 14:09:16 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,6 @@ streamlit.dataframe(my_fruit_list)
 streamlit.multiselect("Pick some fruits:", list (my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruits_list.index)
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruits_list.index))
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
