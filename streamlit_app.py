c5e7dec88efb88e6bef710c0d365de6e2e1491de
SFBADGES <128747881+SFBADGES@users.noreply.github.com>
2023-04-04 11:24:15 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,3 +31,6 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # this commands calls the variable listed above and places it in a dataframe/more readerly form
 streamlit.dataframe(fruityvice_normalized)
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
