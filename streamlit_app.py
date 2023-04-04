a7627ec9adbc30742c7c646a5df7201a5eeba104
SFBADGES <128747881+SFBADGES@users.noreply.github.com>
2023-04-04 11:31:44 -0400

    import requests
    added "import requests" again before the "fruit choice" get command to see if this works or if I should go back up and edit under the first "import requests"

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,7 @@ streamlit.text(fruityvice_response)
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
+import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "fruit choice")
 
 # uses pandas to return just text without quotation marks, commas, etc.
