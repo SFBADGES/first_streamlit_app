a9947080047cc50e7da165b26d9cecf0f3488377
SFBADGES <128747881+SFBADGES@users.noreply.github.com>
2023-04-04 11:57:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "fruit_choice")
+
 
 # uses pandas to return just text without quotation marks, commas, etc.
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
