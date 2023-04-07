c322fdc1e129a288a6b499f33bab6cdb89d3575d
SFBADGES <128747881+SFBADGES@users.noreply.github.com>
2023-04-07 13:21:32 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,3 +48,5 @@ my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
 fruit_additions = streamlit.text_input('What fruit would you like information about?')
 streamlit.write('Thank you for adding', fruit_additions)
+
+my_cur.execute("insert into FRUIT_LOAD_LIST values from ('stream.lit'))
