164f9cab0ef99ee0149a7f1d03a6ce34b2f1a1f7
SFBADGES <128747881+SFBADGES@users.noreply.github.com>
2023-04-07 13:10:23 -0400

    Update streamlit_app.py
    try to add text box for user input but this time include snowflake connector code

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,5 +43,8 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT * from fruit_load_list")
 fruit_additions = streamlit.text_input('What fruit would you like information about?','apple')
 streamlit.write('The user entered ', fruit_additions)
