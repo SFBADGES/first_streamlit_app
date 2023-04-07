e92a15e92177fe0c9668715657babda5913114f4
SFBADGES <128747881+SFBADGES@users.noreply.github.com>
2023-04-07 13:09:15 -0400

    Update streamlit_app.py
    try to add another text box for user input

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,6 @@ my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+fruit_additions = streamlit.text_input('What fruit would you like information about?','apple')
+streamlit.write('The user entered ', fruit_additions)
