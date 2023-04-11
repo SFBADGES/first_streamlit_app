78082e4f7337d8f59bc9987904ea66b177ee5a13
SFBADGES <128747881+SFBADGES@users.noreply.github.com>
2023-04-11 17:08:19 -0400

    Update streamlit_app.py
    move code around so that code that only needs to be executed once is separate from code that needs to be executed multiple times, each time a value is entered.

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,24 @@ streamlit.title('My Mom\'s New Healthy Diner')
 
 
 streamlit.header('Breakfast Favorites')
+#fruit_choice = streamlit.text_input('What fruit would you like information about?')
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#streamlit.dataframe(fruityvice_normalized)
+
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information."
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+   streamlit.dataframe(fruityvice_normalized)            
+      
+except URLError as e:
+   streamlit.error()                 
+
 
 streamlit.text(' 🍚Omega 3 & Blue Berry Oatmeal')
 streamlit.text(' 🧋 Kale, Spinach & Rocket Smoothie')
@@ -27,17 +45,17 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','apple')
-streamlit.write('The user entered ', fruit_choice)
+#fruit_choice = streamlit.text_input('What fruit would you like information about?','apple')
+#streamlit.write('The user entered ', fruit_choice)
 
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 
 # uses pandas to return just text without quotation marks, commas, etc.
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # this commands calls the variable listed above and places it in a dataframe/more readerly form
-streamlit.dataframe(fruityvice_normalized)
+#streamlit.dataframe(fruityvice_normalized)
 streamlit.stop()
 
 
