c10eaf841711e5227e37c99d34506117a73f6436
SFBADGES <128747881+SFBADGES@users.noreply.github.com>
2023-04-11 17:27:00 -0400

    Update streamlit_app.py
    needed to delete streamlit.stop() from earlier to allow new def to work (which is below wehre streamlit.stop() was.

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ except URLError as e:
 #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # this commands calls the variable listed above and places it in a dataframe/more readerly form
 #streamlit.dataframe(fruityvice_normalized)
-streamlit.stop()
+
 
 
 streamlit.header("The fruit load list contains:")
