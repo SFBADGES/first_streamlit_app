9d07b70264ba7a6de06ae2111800e8caaf2c9fd9
SFBADGES <128747881+SFBADGES@users.noreply.github.com>
2023-04-11 17:13:46 -0400

    Update streamlit_app.py
    moved some code around so it matched the instructor's page, which was more logical for the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,23 +9,7 @@ streamlit.title('My Mom\'s New Healthy Diner')
 
 
 streamlit.header('Breakfast Favorites')
-#fruit_choice = streamlit.text_input('What fruit would you like information about?')
-#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#streamlit.dataframe(fruityvice_normalized)
-
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-    streamlit.error("Please select a fruit to get information.")
-  else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)            
-      
-except URLError as e:
-   streamlit.error()                 
 
 
 streamlit.text(' 🍚Omega 3 & Blue Berry Oatmeal')
@@ -45,11 +29,31 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
+
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)            
+      
+except URLError as e:
+   streamlit.error()                 
+
+
 #fruit_choice = streamlit.text_input('What fruit would you like information about?','apple')
 #streamlit.write('The user entered ', fruit_choice)
 
 
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
+
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#streamlit.dataframe(fruityvice_normalized)
+
+
 
 
 # uses pandas to return just text without quotation marks, commas, etc.
